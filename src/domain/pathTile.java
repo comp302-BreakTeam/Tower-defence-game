e7f31c8b02cfee97c5a473d18cccc28f6f2d2fdd
ecesever <ecsever23@ku.edu.tr>
2025-04-19 20:38:27 +0300

    fix_emptyTile has final constants tower and enemy = null. pathTile has final constant tower = null and towerTile has final constant tower = null, these final constants cannot be modified and it's forbidden to have values in the corresponding tiles. pathTile only includes enemies and towerTile only includes towers.

--- src/domain/pathTile.java
+++ src/domain/pathTile.java
@@ -1,5 +1,36 @@
 package domain;
 
-public class pathTile extends Tile{
+import java.util.ArrayList;
 
+public class pathTile extends Tile{
+	private ArrayList<Tile> adjacentTiles = new ArrayList<Tile>();
+	private Enemy enemyOnTile = null;
+	final private Tower tower = null;
+
+	public pathTile(ArrayList<Tile> adjacentTiles, Enemy enemyOnTile) {
+		super();
+		this.adjacentTiles = adjacentTiles;
+		this.enemyOnTile = enemyOnTile;
+	}
+
+	public ArrayList<Tile> getAdjacentTiles() {
+		return adjacentTiles;
+	}
+
+	public void setAdjacentTiles(ArrayList<Tile> adjacentTiles) {
+		this.adjacentTiles = adjacentTiles;
+	}
+
+	public Enemy getEnemyOnTile() {
+		return enemyOnTile;
+	}
+
+	public void setEnemyOnTile(Enemy enemyOnTile) {
+		this.enemyOnTile = enemyOnTile;
+	}
+	
+	public Tower getTower() {
+		return tower;
+	}
+	
 }
