c5b0628a00143fc952184d3f821b362dc0f67f2d
DevrimPacal <147706780+DevrimPacal@users.noreply.github.com>
2025-05-02 02:50:26 +0300

    base structure for Projectile class

--- src/domain/Projectile.java
+++ src/domain/Projectile.java
@@ -3,5 +3,59 @@ package domain;
 
 
 public class Projectile {
+	private Tower source;
+	private Enemy target;
+	private int damage;
+	
+	public Tower getSource() {
+		return source;
+	}
 
+
+	public void setSource(Tower source) {
+		this.source = source;
+	}
+
+
+	public Enemy getTarget() {
+		return target;
+	}
+
+
+	public void setTarget(Enemy target) {
+		this.target = target;
+	}
+
+
+	public int getDamage() {
+		return damage;
+	}
+
+
+	public void setDamage(int damage) {
+		this.damage = damage;
+	}
+
+
+	public Projectile(Tower source, Enemy target, int damage) {
+		this.source = source;
+		this.target = target;
+		this.damage = damage;
+	}
+	
+	public void checkCollision() {
+		
+	}
+	
+	public void getCoordinates() {
+		
+	}
+	
+	public void setDirection(float angle) {
+		
+	}
+	
+	public void applyGravity() {
+		
+	}
 }
