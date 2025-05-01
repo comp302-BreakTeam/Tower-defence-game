7a85a54a158de1bc77d434f9b8aebfa678f01ce6
DevrimPacal <147706780+DevrimPacal@users.noreply.github.com>
2025-05-01 22:35:28 +0300

    Tower minor adjustments

--- src/domain/Tower.java
+++ src/domain/Tower.java
@@ -8,10 +8,47 @@ public abstract class Tower {
     protected int range;
     protected float fireRate;
     protected int cost;
-    protected int position;
+    
+    public Tower(int damage, float fireRate, int cost) {
+    	this.level = 1;
+    	this.damage = damage;
+    	this.fireRate = fireRate;
+    	this.cost = cost;
+    }
+    public int getLevel() {
+		return level;
+	}
 
-    public static void fireProjectile(){
+	public void setLevel(int level) {
+		this.level = level;
+	}
 
+	public int getDamage() {
+		return damage;
+	}
+
+	public void setDamage(int damage) {
+		this.damage = damage;
+	}
+
+	public float getFireRate() {
+		return fireRate;
+	}
+
+	public void setFireRate(float fireRate) {
+		this.fireRate = fireRate;
+	}
+
+	public int getCost() {
+		return cost;
+	}
+
+	public void setCost(int cost) {
+		this.cost = cost;
+	}
+
+	public static void fireProjectile(){
+    	
     }
 
     public static void upgradeTower(){
