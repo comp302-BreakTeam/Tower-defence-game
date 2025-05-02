e775396b295a20c970db95a2fb7d5c59776cf260
DevrimPacal <147706780+DevrimPacal@users.noreply.github.com>
2025-05-02 19:08:04 +0300

    random enemies

--- src/domain/Wave.java
+++ src/domain/Wave.java
@@ -5,16 +5,21 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Random;
 
 public class Wave {
+	public Random rand = new Random(2);
 	private Queue<Enemy> enemies = new LinkedList<Enemy>();
 
 	public Wave(int waveSize) {
 		int i=0;
-		for(i=0; i<waveSize/2; i++) {
-			enemies.add(new Goblin());
-		}for(int k=i; k<waveSize; k++) {
-			enemies.add(new Knight());
+		for(i=0; i<waveSize; i++) {
+			if(rand.nextBoolean()) {
+				enemies.add(new Goblin());
+			}
+			else {
+				enemies.add(new Knight());
+			}
 		}
 		
 		
