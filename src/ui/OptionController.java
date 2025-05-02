be014ca1b9aca2305345051e99b3f2822a5e2e64
DevrimPacal <147706780+DevrimPacal@users.noreply.github.com>
2025-05-02 14:08:14 +0300

    added new options

--- src/ui/OptionController.java
+++ src/ui/OptionController.java
@@ -1,10 +1,12 @@
 package ui;
 
 import domain.Player;
+
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.control.Slider;
 import javafx.stage.Stage;
 
@@ -12,9 +14,15 @@ public class OptionController {
 	public Player player;
 	private Scene previousScene;
 	private static double savedVolume = 50;
-	private static String savedGraphics = "Medium";
+	private static int savedLives = 20;
+	private static double savedSpeed = 1.0;
+	private static int savedGold = 500;
+	private static int savedWave = 1;
 	private double originalVolume;
-	private String originalGraphicsQuality;
+	private int originalLives;
+	private double originalSpeed;
+	private int originalGold;
+	private int originalWave;
 
 	public void setPlayer(Player player) {
 		this.player= player;
@@ -25,33 +33,39 @@ public class OptionController {
         this.previousScene = scene;
     }
 @FXML private Slider volumeSlider;
-@FXML private ComboBox<String> graphicsComboBox;
+@FXML private Spinner<Integer> livesSpinner;
+@FXML private Spinner<Double> enemySpeedSpinner;
+@FXML private Spinner<Integer> goldSpinner;
+@FXML private Spinner<Integer> waveSpinner;
 @FXML private Button saveButton;
 @FXML private Button discardButton;
 
 @FXML
 public void initialize() {
-    graphicsComboBox.getItems().addAll("Low", "Medium", "High");
-    graphicsComboBox.setValue("Medium");
     
+    livesSpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 99, savedLives));
+    goldSpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 10000, savedGold));
+    waveSpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 100, savedWave));
+    enemySpeedSpinner.setValueFactory(new SpinnerValueFactory.DoubleSpinnerValueFactory(0.5, 5.0, savedSpeed, 0.5));
+	
     volumeSlider.setValue(savedVolume);
-    graphicsComboBox.setValue(savedGraphics);
     
-    originalVolume = savedVolume;
-    originalGraphicsQuality = savedGraphics;
+    originalSpeed = savedSpeed;
+    originalLives = savedLives;
+    originalGold = savedGold;
+    originalWave = savedWave;
 
     
     javafx.application.Platform.runLater(() -> {
         originalVolume = volumeSlider.getValue();
-        originalGraphicsQuality = graphicsComboBox.getValue();
     });
 
-    originalVolume = volumeSlider.getValue();
-    originalGraphicsQuality = graphicsComboBox.getValue();
-
     saveButton.setOnAction(e -> {
-        savedVolume = volumeSlider.getValue();
-        savedGraphics = graphicsComboBox.getValue();
+    	savedVolume = volumeSlider.getValue();
+        savedLives = livesSpinner.getValue();
+        savedSpeed = enemySpeedSpinner.getValue();
+        savedGold = goldSpinner.getValue();
+        savedWave = waveSpinner.getValue();
 
         if (previousScene != null) {
             Stage stage = (Stage) saveButton.getScene().getWindow();
@@ -60,8 +74,11 @@ public void initialize() {
     });
 
     discardButton.setOnAction(e -> {
-        volumeSlider.setValue(originalVolume);
-        graphicsComboBox.setValue(originalGraphicsQuality);
+    	volumeSlider.setValue(originalVolume);
+        livesSpinner.getValueFactory().setValue(originalLives);
+        enemySpeedSpinner.getValueFactory().setValue(originalSpeed);
+        goldSpinner.getValueFactory().setValue(originalGold);
+        waveSpinner.getValueFactory().setValue(originalWave);
 
         if (previousScene != null) {
             Stage stage = (Stage) discardButton.getScene().getWindow();
