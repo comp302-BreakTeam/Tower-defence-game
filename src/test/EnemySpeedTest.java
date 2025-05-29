c2b289cc92dc77c784eb60da8bc8f5196a1a484c
cserbester <cserbester23@ku.edu.tr>
2025-05-29 23:38:57 +0300

    enemyspeed test

--- src/test/EnemySpeedTest.java
+++ src/test/EnemySpeedTest.java
@@ -1,19 +1,26 @@
 package test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import domain.Knight;
+import javafx.embed.swing.JFXPanel;
 
-public class EnemyTest {
+public class EnemySpeedTest {
 	private Knight knight;
 	
-	
+	 @BeforeEach
+	    public void setup() {
+	        new JFXPanel();
+	        knight = new Knight(); 
+	 }
 	@Test
     public void testBeSlowedReducesSpeed() {
-		knight = new Knight();
+		
 		float originalSpeed = knight.getSpeed();
 		knight.beSlowed();
 		assertEquals(originalSpeed * 0.8f, knight.getSpeed(), 0.01f);
@@ -21,18 +28,19 @@ public class EnemyTest {
 	
 	@Test
     public void testBeSlowedSetsSlowedFlag() {
-		knight = new Knight();
+		
 		knight.beSlowed();
 		assertTrue(knight.isSlowed());
 	}
 	
 	@Test
     public void testSpeedRestoresAfterDelay() throws InterruptedException {
-		knight = new Knight();
+		
 		float originalSpeed = knight.getSpeed();
 		knight.beSlowed();
 		Thread.sleep(4500);
 		assertFalse(knight.isSlowed());
+		assertEquals(originalSpeed, knight.getSpeed(), 0.01f);
 	}
 	
 
