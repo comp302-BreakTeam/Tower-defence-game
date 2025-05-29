e96b63cf23a758892027761b6e7c2a13bf986736
SarpEren <147648908+SarpEren@users.noreply.github.com>
2025-05-30 00:17:36 +0300

    Create UpgradeTowerTest.java

---
+++ src/test/UpgradeTowerTest.java
@@ -0,0 +1,43 @@
+package test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import domain.Archer_Tower;
+import domain.Tower;
+
+public class UpgradeTowerTest {
+    Tower tower;
+
+    @BeforeEach
+    public void setUp() {
+        tower = new Archer_Tower();
+        //Damage of Archer Tower 10
+        //Fire Rate of Archer Tower 6f
+        //Cost of Archer Tower 100
+    }
+    
+    @Test
+    public void initialLevelTest() {
+    	assertEquals(1, tower.getLevel());
+    }
+    
+    @Test
+    public void testUpgradeFireRate() {
+        tower.upgradeTower();
+        assertEquals(12f, tower.getFireRate(), 0.0001);
+    }
+
+    @Test
+    public void testUpgradeLevel() {
+        tower.upgradeTower();
+        assertEquals(2, tower.getLevel());
+    }
+
+    @Test
+    public void testUpgradeRange() {
+        tower.upgradeTower();
+        assertEquals(7.5f, tower.getRange(), 0.0001);
+    }
+}
+
